8c5a25ed36bcbb025b0cf8bb2211391b93b3d7d8
ZhaoHongzcyh <15984097564@163.com>
2018-07-19 23:50:25 +0800

    update mailList page

--- pages/mailList/mailList.js
+++ pages/mailList/mailList.js
@@ -1,17 +1,35 @@
 // pages/mailList/mailList.js
+const app = getApp();
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    equipment:{
+      src:"/pages/mailList/img/equipment.png",
+      title:"我的设备"
+    },
+    list:[
+      {
+        src:"/pages/mailList/img/head.png",
+        title:"我的好友",
+        num:2
+      },
+      {
+        src: "/pages/mailList/img/head.png",
+        title: "自定义分组",
+        num: 0
+      },
+      {
+        src: "/pages/mailList/img/head.png",
+        title: "陌生人",
+        num: 0
+      }
+    ],
+    group:{
+      src: "/pages/mailList/img/head.png",
+      title: "我的群组",
+      num: 1
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onLoad:function (options) {
   
   },
 
@@ -19,27 +37,6 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
   
   },
 
@@ -54,13 +51,6 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
   
   }
 })
